5d954428bf9e13d483142b61d030248e501925c2
Dwight Van Tuyl <dvantuyl@gmail.com>
2024-09-14 10:27:40 -0700

    add typing to SseMessage module

--- lib/simple_sse/message.ex
+++ lib/simple_sse/message.ex
@@ -4,19 +4,27 @@ defmodule SimpleSse.SseMessage do
   alias Phoenix.PubSub
   alias SimpleSse.SseMessage
 
+  @type t :: %SseMessage{event: String.t(), data: String.t()}
+
+  @spec chunk_to(SseMessage.t(), Plug.Conn.t()) :: Plug.Conn.t()
   def chunk_to(%SseMessage{} = msg, conn) do
     Plug.Conn.chunk(conn, "event: #{msg.event}\r\ndata: #{msg.data}\r\n\n")
     conn
   end
 
+  @spec broadcast(String.t(), String.t()) :: :ok
   def broadcast(topic, message) do
     PubSub.broadcast(SimpleSse.PubSub, topic, {:message, message})
+    :ok
   end
 
+  @spec subscribe(String.t()) :: :ok
   def subscribe(topic) do
     PubSub.subscribe(SimpleSse.PubSub, topic)
+    :ok
   end
 
+  @spec stream_to(Plug.Conn.t(), module(), (String.t() -> String.t())) :: Plug.Conn.t()
   def stream_to(conn, component, render) do
     receive do
       {:message, message} ->
